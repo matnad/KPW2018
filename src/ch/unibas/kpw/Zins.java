dc5ede2e9167fe5c05c9304c588b1b8ec2ef07d1
nadmat00 <nadmat00@WWZ-16-PC-81.wwz.unibas.ch>
2018-10-15 16:04:26 +0200

    uebung4, zins

---
+++ src/ch/unibas/kpw/Zins.java
@@ -0,0 +1,20 @@
+package ch.unibas.kpw;
+
+public class Zins {
+
+	public static void main(String[] args) {
+		int jahre = getAmount(3.5);
+		System.out.println(jahre);
+
+	}
+	
+	static int getAmount(double p) {
+		double kapital = 100;
+		int jahr = 0;
+		while(kapital < 1000) {
+			kapital *= (1+p/100);
+			jahr++;
+		}
+		return jahr;
+	}
+}
