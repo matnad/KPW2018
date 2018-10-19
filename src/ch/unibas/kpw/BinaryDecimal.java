9b3ce930e1a315db233ba87e92e4442c706b5fe8
nadmat00 <nadmat00@WWZ-16-PC-88.wwz.unibas.ch>
2018-10-19 12:46:45 +0200

    bin2dec

--- src/ch/unibas/kpw/BinaryDecimal.java
+++ src/ch/unibas/kpw/BinaryDecimal.java
@@ -3,14 +3,25 @@ package ch.unibas.kpw;
 public class BinaryDecimal {
 	public static void main(String[] args) {
 		
-		char[] binary_array = args[0].toCharArray();
+		// Get String input by using javax.swing
+		String in_x;
+        try {
+            in_x = javax.swing.JOptionPane.showInputDialog(null,
+                            "Binary to Decimal", "Enter sequence of 0's and 1's.");
+        } catch (Exception e) {
+            in_x = "0";
+        }
+		
+        char[] binary_array = in_x.toCharArray();
 		
 		int dec = 0;
-		int bin_len = binary_array.length; 
+		int a = 1;
+		int bin_len = binary_array.length;
 		
 		for(int i=bin_len-1; i >= 0; i--) {
 			int bn = Character.getNumericValue(binary_array[i]);	
-			dec += Math.pow(2,bin_len-1-i)*bn;
+			dec += bn*a;
+			a *= 2;
 		}
 		
 		System.out.println(dec);
