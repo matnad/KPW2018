8334fc3049979d097411d848e8676c1897a51101
nadmat00 <nadmat00@WWZ-16-PC-80.wwz.unibas.ch>
2018-11-09 15:45:50 +0100

    v8

--- src/ch/unibas/kpw/TestQueue.java
+++ src/ch/unibas/kpw/TestQueue.java
@@ -5,9 +5,12 @@ public class TestQueue {
 	public static void main(String[] args) {
 		// Queue q = new Queue(10);
 		java.util.Queue q = new java.util.LinkedList();
-		q.put(3);
-		q.put(6);
-		int x = q.get(); // x == 3 int y = q.get();// y == 6
+		q.add(3);
+		q.add(6);
+		// q.put(3);
+		// q.put(6);
+		//int x = q.get(); // x == 3 int y = q.get();// y == 6
+		int x = (int) q.remove();
 		System.out.println(x);
 	}
 
